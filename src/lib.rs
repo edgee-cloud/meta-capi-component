8e7d92e8a6fd011fc32d38e59177785c08cd2d75
Sacha Morard <2254275+SachaMorard@users.noreply.github.com>
2025-01-23 06:59:06 +0100

    fix: data-collection world

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use crate::exports::edgee::protocols::data_collection::{
 };
 use meta_payload::{parse_value, MetaEvent, MetaPayload};
 
-wit_bindgen::generate!({world: "edgee", path: "wit", with: { "edgee:protocols/data-collection": generate }});
+wit_bindgen::generate!({world: "data-collection", path: "wit", generate_all});
 
 export!(MetaComponent);
 
